57f4fb75070176cf6e6061f471dc6a08fbb44d51
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-08-04 15:00:56 +0700

    UPDATE: Added authentication features (signup, signin, forgot password)

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
-
 
 // TODO: middleware ketika user login ke auth admin
 Route::middleware(['custom.throttle:5,1'])->group(function () {
-    // untuk auth
+
+    // <!-- Test Throttle pakek ini -->
+    // Route::get('/test-throttle', function () {
+    //     return response()->json(['message' => 'OK']);
+    // });
+    // <!-- Test Throttle pakek ini -->
+
+    Route::post('/signup', [RegisterController::class, 'signUp']);
+    Route::post('/signin', [LoginController::class, 'signIn']);
+    Route::post('/signup-verify-otp', [RegisterController::class, 'signUpVerifyOTP']);
+    Route::post('/send-otp', [ForgotPasswordController::class, 'sendOTP']);
+    Route::post('/verify-otp', [ForgotPasswordController::class, 'verifyOTP']);
+    Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword']);
 });
 
-Route::middleware(['auth:sanctum', 'custom.throttle:60,1'])->group(function () {
-    // logout
-    // user info
+Route::middleware(['auth:sanctum', 'custom.throttle:20,1'])->group(function () {
+    Route::get('/logout', [LoginController::class, 'logout'])->middleware('web');
+    Route::get('/user-info', [LoginController::class, 'getUserInfo']);
 
     Route::group(['prefix' => 'mamura'], function () {
         Route::group(['prefix' => 'public-request'], function () {
@@ -21,6 +35,7 @@ Route::middleware(['auth:sanctum', 'custom.throttle:60,1'])->group(function () {
         Route::group(['prefix' => 'admin', 'middleware' => ['verified.role:admin']], function () {
             // route module master data
         });
+
         Route::group(['middleware' => ['verified.role:users']], function () {
             // route module user
         });
