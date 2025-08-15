81c41b4d8aab513bc81268c82caa226d869df8ca
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-08-15 23:45:54 +0700

    UPDATE: Adding API for get blog by slug in public request

--- app/Http/Controllers/Public/PublicRequestController.php
+++ app/Http/Controllers/Public/PublicRequestController.php
@@ -695,7 +695,7 @@ class PublicRequestController extends Controller
     {
         try {
             $blog = Blog::where('slug', $slug)->first();
-            if ($blog->isEmpty()) {
+            if (!$blog) {
                 return response()->json(
                     new WithoutDataResource(
                         Response::HTTP_NOT_FOUND,
