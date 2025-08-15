33756f0146f6613968db923a14f04376a05daca8
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-08-15 23:48:21 +0700

    UPDATE: Adding API for get blog by slug in public request

--- app/Http/Controllers/Public/PublicRequestController.php
+++ app/Http/Controllers/Public/PublicRequestController.php
@@ -707,7 +707,7 @@ class PublicRequestController extends Controller
                 );
             }
 
-            $data = BlogResource::collection($blog)
+            $data = BlogResource::collection($slug)
                 ->map(fn($item) => collect($item)->except(['created_at', 'updated_at', 'deleted_at']));
 
             return response()->json(
