2bb9c58808eff4dca3940d6b03d89f49d63206b2
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-08-17 08:43:59 +0700

    FIXED: Updated code uploadInternalImage to delete documents

--- app/Http/Controllers/Public/PublicRequestController.php
+++ app/Http/Controllers/Public/PublicRequestController.php
@@ -968,7 +968,7 @@ class PublicRequestController extends Controller
             // Ambil trio id, file_id, file_url untuk respons
             $uploadDocumentIds = [];
             if (!empty($uploadDocumentIds)) {
-                $documents = Document::whereIn('id', $uploadDocumentIds)
+                $uploadDocumentIds = Document::whereIn('id', $uploadDocumentIds)
                     ->get(['id', 'file_id', 'file_url'])
                     ->map(fn($d) => [
                         'id'       => $d->id,
@@ -987,7 +987,7 @@ class PublicRequestController extends Controller
                     'Berhasil memproses gambar internal BE.',
                     [
                         'deleted_document_ids' => array_map('intval', $deleteIds),
-                        'uploaded_documents'   => $documents,
+                        'uploaded_documents'   => $uploadDocumentIds,
                     ]
                 ),
                 Response::HTTP_CREATED
